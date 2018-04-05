026b6f1973e2b2a77e3353f2bd6b6d4d3e4ba1ae
Daniel <daniel.seidel.work@gmail.com>
2018-04-05 12:26:28 +0200

    changed program structure

--- Assets/Daniel/Scripts/Movement/PlayerMovement.cs
+++ Assets/Daniel/Scripts/Movement/PlayerMovement.cs
@@ -2,24 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AttackBehavior))]
+[RequireComponent(typeof(AttackBehavior)), RequireComponent(typeof(Entity))]
 public class PlayerMovement : Movement {
     
-    public float movementSpeed = 5.0f;
-    
     private AttackBehavior attackBehavior;
+    private Entity entityScript;
 
-    private float playerWidth, playerHeight;
+    //private float playerWidth, playerHeight;
 
 	// Use this for initialization
 	void Start () {        
         attackBehavior = GetComponent<AttackBehavior>();
-
+        entityScript = GetComponent<Entity>();
     }
 
     // Update is called once per frame
 	void Update () {
-		Move (movementSpeed, Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+		Move (entityScript.movementSpeed, Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
 		if (Input.GetKeyDown ("space")) {
 			attackBehavior.SpawnWeapon (direction);
 		}
